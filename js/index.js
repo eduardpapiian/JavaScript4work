c4f99e30013853e67a30a0c269ba68f623a409ec
Eduard Papiian <eduardpapiian@gmail.com>
2018-05-30 18:00:21 +0000

    create index.html index.js

---
+++ js/index.js
@@ -0,0 +1,36 @@
+//   const userName = prompt ('Enter your name!');
+//   const greet = 'Hello ' + userName
+//   alert (greet);
+
+// const message = 'Вітаємо на нашому сайті!'
+// const greetings = 'Hello' + '!';
+// alert(greetings);
+// alert(message);
+
+// const userAge = parseInt(prompt ('Enter your age!'));
+// if(userAge < 18) {
+//     alert('Ти Малий');
+// }else if (userAge < 80){
+//     alert('Adult');
+// } else {
+//     alert('Стврый')
+// }
+
+// function sum(a, b){
+//     const result = a + b;
+//     return result;
+// }
+
+// let sumOfNums =sum(10, 20);
+// alert(sumOfNums);
+// sumOfNums = sum(20, 30);
+// alert (sumOfNums);
+
+
+function sumOfThree(a, b, c){
+    const result = a + b +c;
+    return result;
+}
+
+let sumThree =sumOfThree(10, 20, 30);
+alert(sumThree);
